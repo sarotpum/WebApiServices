cdb653aa3ec3e887439857e5b84cdd364882e90f
sr.pum <sr.pummanee@gmail.com>
2024-11-24 21:30:39 +0700

    modifile bokmodel

---
+++ SharedService/Models/Books/BookModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SharedService.Models.Books
+{
+    public class BookModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+    }
+}
